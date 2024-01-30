95de0becab1b24113d91183dd23c351785220a7c
antonypetriumili <hokdart001@gmail.com>
2024-01-30 17:17:59 +0200

    add user settings

---
+++ Mean Square Error/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mean_Square_Error
+{
+    internal class Program
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            return firstArray.Zip(secondArray, (a, b) => (a - b) * (a - b))
+                     .Average();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6 }));
+        }
+    }
+}
